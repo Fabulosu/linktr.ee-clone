35e188950428f826894d2bae1ff0397e7f029f54
Edward Batalan <batalanedward@gmail.com>
2024-05-21 00:09:34 +0300

    minor homepage changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import RotatingImage from "@/components/ui/main-image";
 import { ChangeEvent, useState } from "react";
 
 export default function Home() {
@@ -8,24 +9,26 @@ export default function Home() {
   const [name, setName] = useState<string>('');
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length < 0) {
-      setName("")
-    } else {
-      setName(e.target.value);
-    }
+    setName(e.target.value);
   };
 
   return (
-    <main className="flex flex-row items-center justify-center px-5 md:px-20 mt-32 md:mt-40 w-full lg:w-3/4 z-[20]">
-      <div className="container h-full w-full lg:w-3/4 flex flex-col gap-5 justify-center m-auto text-start">
+    <main className="flex flex-row items-center justify-center px-5 md:px-8 mt-32 md:mt-40 w-full lg:w-3/4 z-[20] container">
+      <div className="h-full w-full lg:w-3/4 flex flex-col gap-5 justify-center m-auto text-start">
         <div className="flex flex-col gap-6 mt-6 text-7xl font-bold text-foreground max-w-[600px] w-auto h-auto">
           <h1>Everything you are. In one, simple link in bio.</h1>
         </div>
         <p className="text-lg text-muted-foreground my-5 max-w-[600px]">Join 40M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
         <div className="flex flex-row">
-          <Input className="max-w-[200px] mr-3" placeholder="linktr.ee/yourname" value={`linktr.ee/${name.replace('linktr.ee/', "")}`} onChange={handleInputChange} />
+          <div className="flex items-center">
+            <span className="text-default pl-[10px] pt-[1px]">linktr.ee/</span>
+            <Input className="-ml-[73px] max-w-[200px] mr-3 bg-transparent pl-[73px] text-md" placeholder="yourname" value={name} onChange={handleInputChange} maxLength={10} />
+          </div>
           <Button variant="default">Claim your Linktree</Button>
         </div>
+      </div>
+      <div className="w-full h-full md:flex justify-center items-center hidden">
+        <RotatingImage src="/phonefront.png" alt="" />
       </div>
     </main>
   );
---
+++ src/components/ui/main-image.tsx
@@ -0,0 +1,65 @@
+import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { useState, useEffect } from 'react';
+
+interface RotatingImageProps {
+    src: string;
+    alt: string;
+}
+
+const RotatingImage: React.FC<RotatingImageProps> = ({ src, alt }) => {
+    const [imageCenter, setImageCenter] = useState({ x: 0, y: 0 });
+    const [innerWidth, setInnerWidth] = useState(0);
+    const [innerHeight, setInnerHeight] = useState(0);
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(0);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setInnerWidth(window.innerWidth);
+            setInnerHeight(window.innerHeight);
+        }
+        const handleMouseMove = (event: MouseEvent) => {
+            mouseX.set(event.clientX);
+            mouseY.set(event.clientY);
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, [mouseX, mouseY]);
+
+    const rotateX = useTransform(mouseY, [0, innerHeight], [15, -15]);
+    const rotateY = useTransform(mouseX, [0, innerWidth], [-15, 15]);
+
+    const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement>) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        setImageCenter({
+            x: rect.left + rect.width / 2,
+            y: rect.top + rect.height / 2,
+        });
+    };
+
+    return (
+        <motion.div
+            className='ml-60'
+            style={{
+                perspective: 250,
+            }}
+        >
+            <motion.img
+                src={src}
+                alt={alt}
+                style={{
+                    rotateX,
+                    rotateY,
+                }}
+                className="w-[400px] h-[400px]"
+                onLoad={handleImageLoad}
+            />
+        </motion.div>
+    );
+};
+
+export default RotatingImage;
