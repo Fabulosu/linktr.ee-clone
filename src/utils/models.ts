b0b7c731e6b104604fc97bf84d759daa1d3471a2
Edward Batalan <batalanedward@gmail.com>
2024-05-22 18:15:05 +0300

    restructured db models

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -10,10 +10,28 @@ const userSchema = new Schema({
     password: reqString,
     email: reqString,
     avatarURL: String,
+    backgroundURL: String,
     activated: { type: Number, required: true },
     uuid: { type: String, required: true },
+    name: String,
+    settings: Array,
+}, { timestamps: true });
+
+const linkSchema = new Schema({
+    title: reqString,
+    url: reqString,
+    enabled: Boolean,
+    thumbnail: String,
+    settings: Array,
+}, { timestamps: true });
+
+const headerSchema = new Schema({
+    title: reqString,
+    enabled: Boolean,
 }, { timestamps: true });
 
 const UserModel = models.User || model("User", userSchema);
+const LinkModel = models.Link || model("Link", linkSchema);
+const HeaderModel = models.Header || model("Header", headerSchema);
 
-export { UserModel };
+export { UserModel, LinkModel, HeaderModel };
